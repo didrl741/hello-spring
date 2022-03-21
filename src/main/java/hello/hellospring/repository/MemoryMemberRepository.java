3f4374cb85caa768dc3f717f21c2817605920cc5
Yanggi Song <didrl741@gmail.com>
2022-03-21 12:06:40 +0900

    Test : 메서드들이 제대로 동작하는지 미리 테스트
    두 값이 같은지 확인할 때: Assertions.assertThat(~).isEqualTo(~2)
    테스트 클래스를 실행할 때: 각 테스트메서드들이 순서에 의존하지 않기 위해
    메서드가 끝날 때 마다 AfterEach()를 이용해서 clear 해준다.

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
-    private static long sequence = 0L;
+    private static long sequence = 0L;      // 0, 1, 2.. 키 값을 생성하는 역할.
 
     @Override
     public Member save(Member member) {
@@ -18,19 +18,23 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public Optional<Member> findById(Long id) {
-        //return store.get(id);     이렇게하면 null이면 null return.
+        //return store.get(id);     이렇게하면 반환될 값이 없으면 null return -> 곤란해짐.
         return Optional.ofNullable(store.get(id));  // null 이여도 감쌀수 있다.
     }
 
     @Override
     public Optional<Member> findByName(String name) {
         return store.values().stream()
-                .filter(member -> member.getName().equals(name))
+                .filter(member -> member.getName().equals(name))    // map에서 루프돌면서 name인지 확인.
                 .findAny();
     }
 
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }
---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,71 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+// 전체 test 돌리면, 각 테스트 메서드들은 순서에 맞지 않게 실행됨.
+// -> 순서에 의존적이게 설계하지 말자. -> 각 Test메서드가 끝나면 repository를 비워주자!
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    // 콜백 메서드. 각 메서드들이 끝나면 자동으로 동작.
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("yanggi");
+
+        repository.save(member);
+
+        // Optional에서 값 꺼낼 때 -> get 이용.
+        Member result = repository.findById(member.getId()).get();
+
+        // 검증. 다르다면 빨간불이 뜸.
+        Assertions.assertEquals(member, result);
+
+        // 두 번째 검증 방법. 이게 좀더 가시성. static import 하면 지저분한 부분 위로 올라감.
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
