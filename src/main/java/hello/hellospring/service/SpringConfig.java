b2d989a88831268ded4c01ddcbff348010e5e176
Yanggi Song <didrl741@gmail.com>
2022-03-26 16:27:21 +0900

    db와 spring 연결하기
    build.gradle에서 2줄 추가
    application.properties에서 3줄 추가
    
    Jdbc Template방식: 순수 Jdbc보다 간결하다.
    jdbcTemplate객체에 넣고 빼오는 방식이다.
    구체적인 코드는 아직 익숙치 않다.

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,12 +1,23 @@
 package hello.hellospring.service;
 
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource datasource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.datasource = dataSource;
+    }
 
     // 스프링 빈에 등록.
     @Bean
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        return new JdbcTemplateMemberRepository(datasource);
     }
 }
